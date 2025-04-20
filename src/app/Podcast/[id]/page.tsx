6d27871e0d0bca0ac30af7b156d953cc7237c5d5
Chisom Ohanu <148290753+Chiblessed@users.noreply.github.com>
2025-04-20 05:27:32 +0100

    done

--- src/app/Podcast/[id]/page.tsx
+++ src/app/Podcast/[id]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React,{useState, useEffect} from 'react';
+import React,{useState, useEffect, Suspense} from 'react';
 import Image from 'next/image';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useParams  } from 'next/navigation';
@@ -312,4 +312,11 @@ for (let i = startPage; i <= endPage; i++) {
   )
 }
 
-export default PodcastDetails
+export function PodcastDetailPage() {
+  return (
+    
+    <Suspense>
+      <PodcastDetails />
+    </Suspense>
+  )
+}
